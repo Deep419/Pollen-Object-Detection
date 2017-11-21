bb1d2f3911fd1770da60596e7330370dd660e8c9
UNCCHARLOTTE-NT\dghaghar <21269458+Deep419@users.noreply.github.com>
2017-11-21 17:05:53 -0500

    Changed patch_create script to include varying size of big_images.

--- patch_create.m
+++ patch_create.m
@@ -25,7 +25,11 @@ GT_data = sortrows(GT_data,'id','ascend');
 
 %% 3. Load Images and match sizes
 images = dir(fullfile('big_image','*.tif'));
-% remove whitespaces
+if size(images,1)==0
+     error('Please add images to big_image folder');
+     quit;
+end
+%% 4. Rename Image files
 for i = 1:size(images,1)
     fn = images(i).name;
     nn = strrep(fn,' ','_');
@@ -35,166 +39,20 @@ for i = 1:size(images,1)
 end
 clear fn nn;
 
-if ~(size(GT_data,1) == size(images,1))
-    error('Number of Images in Big_Image folder do not match Size of GT.');
-else
-    %% 4. Rename Image files
-    
-    
-    %% 5. Get Image Size information
-    for i=1:size(GT_data,1)
-        id_num = GT_data.id{i};
-        %if patch folder already exists, skip creating patches for that one
-        folder_name = fullfile('patches',id_num);
-        mkdir(folder_name);
-        
-        if exist(fullfile(folder_name,'gt_data_aug.mat')) == 2
-            fprintf('Patches already exists : %s\n',id_num);
-            continue;
-        end
-        fprintf('Dividing %s : ',id_num);
-        current_image = GT_data.imageFilename{i};
-        I=imread(current_image);
-        [Row, Column, ~]=size(I);
-        
-        rowLines = ceil(Row/1000); %number of lines dividing by height
-        columnLines = ceil(Column/1000);  %number of lines dividing by width
-        
-        %% 6. Create array of Row and Column Lines
-        R = [];
-        C = [];
-        %create an array of Row cropping lines
-        R(1)=floor(Row/rowLines);
-        for k=2:rowLines-1
-            R(k)=R(k-1)+floor(Row/rowLines);
-        end
-        R = [1 R Row]; clear k;
-        
-        %create an array of Column cropping lines
-        C(1)=floor(Column/columnLines);
-        for k=2:columnLines-1
-            C(k)=C(k-1)+floor(Column/columnLines);
-        end
-        C = [1 C Column]; clear k;
-        
-        x = mat2cell(I, diff([0 R(2:end)]) , diff([0 C(2:end)]) , 3);
-        
-        %check if boxes crosses the crop lines
-        %split GT for each cropped image
-        bbox = cell(size(x));
-        extra = [];
-        box = GT_data.bbox{i};%{k,index};
-        %box = box{1};
-        %a=1; %a - index for boxes on cropping lines
-        for m=1:size(box,1)     %m - number of bboxes for each picture
-            [bbox,e] = patch_script.bboxCalculation(box(m,:),C,R,bbox_intersection,bbox);
-            extra = [extra; e];
-            
-        end
-        
-        %% Visualize Entire Image with Patch GT
-        bbox_o = cell(size(x));
-        for ii = 1:size(bbox_o,1)
-            for jj = 1:size(bbox_o,2)
-                if ~isempty(bbox{ii,jj})
-                    bbox_o{ii,jj}=[bbox{ii,jj}(:,1)+C(jj)-1 bbox{ii,jj}(:,2)+R(ii)-1 bbox{ii,jj}(:,3) bbox{ii,jj}(:,4)];
-                end
-            end
-        end
-        img = I;
-        for ro = 2:numel(R)-1
-            img = insertShape(img,'Line',[1 R(ro) Column R(ro)],'LineWidth',10,'Color','white');
-        end
-        for co = 2:numel(C)-1
-            img = insertShape(img,'Line',[C(co) 1 C(co) Row],'LineWidth',10,'Color','white');
-        end
-        img = insertShape(img,'rectangle',extra,'color','black','LineWidth',10);
-        % To vislize extra
-        %figure;imshow(img);
-        
-        sz = 0;
-        color = jet(size(bbox,1)*size(bbox,2)*5);
-        for ii = 1:size(bbox,1)
-            for jj = 1:size(bbox,2)
-                if ~isempty(bbox{ii,jj})
-                    img = insertShape(img,'rectangle',bbox_o{ii,jj},'color', ...
-                        color(sub2ind([size(bbox,1) size(bbox,2)],ii,jj)*5,:)*255,'LineWidth',10);
-                    sz = sz + size(bbox_o{ii,jj},1);
-                end
-            end
-        end
-        %figure;imshow(img);
-        
-        
-        % Make patches linear
-        x_temp = x';
-        x_temp=x_temp(:);
-        imageFilename = {};
-        pollen = {};
-        
-        bbox = bbox';
-        bbox = bbox(:);
-        fprintf('done\n');
-        
-        
-        other_scripts.textprogressbar(sprintf('Creating patches for ID %s : ',id_num));
-        progressBar = numel(1:4:size(x_temp,1)*4);
-        counter = 1;
-        %% add aug script here
-        for aug_i = 1:4:size(x_temp,1)*4%15000
-            
-            patch_image = x_temp{ceil(aug_i/4)};
-            loc = fullfile(folder_name , [id_num '_pat_' num2str(ceil(aug_i/4)) '_aug_']);
-            bbox_per = bbox{ceil(aug_i/4)};
-            [Row,Column,~] = size(patch_image);
-            %[R,C,~]=size(I);
-            fn = [loc '0' '.png'];
-            imageFilename{aug_i,1} = fn;
-            pollen{aug_i,1} = bbox_per;
-            %figure;imshow(insertShape(I,'rectangle',bbox));
-            imwrite(patch_image, fn);
-            
-            I_down = flipud(patch_image);
-            bbox_down = [];
-            if ~isempty(bbox_per)
-                bbox_down = [bbox_per(:,1) Row-(bbox_per(:,2)+bbox_per(:,4)-2) bbox_per(:,3) bbox_per(:,4)];
-            end
-            %figure;imshow(insertShape(I_down,'rectangle',bbox_down));
-            fn = [loc 'ud' '.png'];
-            imageFilename{aug_i+1,1} = fn;
-            pollen{aug_i+1,1} = bbox_down;
-            imwrite(I_down,fn);
-            
-            I_side = fliplr(patch_image);
-            bbox_side = [];
-            if ~isempty(bbox_per)
-                bbox_side = [Column-(bbox_per(:,1)+bbox_per(:,3)-2) bbox_per(:,2) bbox_per(:,3) bbox_per(:,4)];
-            end
-            %figure;imshow(insertShape(I_side,'rectangle',bbox_side));
-            fn = [loc 'lr' '.png'];
-            imageFilename{aug_i+2,1} = fn;
-            pollen{aug_i+2,1} = bbox_side;
-            imwrite(I_side,fn);
-            
-            I_diag = fliplr(I_down);
-            bbox_diag = [];
-            if ~isempty(bbox_per)
-                bbox_diag = [Column-(bbox_down(:,1)+bbox_down(:,3)-2) bbox_down(:,2) bbox_down(:,3) bbox_down(:,4)];
-            end
-            %figure;imshow(insertShape(I_diag,'rectangle',bbox_diag));
-            fn = [loc 'diag' '.png'];
-            imageFilename{aug_i+3,1} = fn;
-            pollen{aug_i+3,1} = bbox_diag;
-            imwrite(I_diag,fn);
-            other_scripts.textprogressbar((counter/progressBar)*100);
-            counter = counter + 1;
-        end
-        other_scripts.textprogressbar('done');
-        
-        bbox = pollen; clear pollen;
-        data_aug = table(imageFilename, bbox);
-        
-        imwrite(img,fullfile(folder_name,['vis_big-' id_num '.png']));
-        save(fullfile(folder_name,'gt_data_aug.mat'), 'data_aug');
-    end
-end
+%if size(GT_data,1) > size(images,1)
+    for i = 1:size(images,1)
+       idx = find(strcmp(['big_imag\' images(i).name],GT_data.imageFilename));
+       if ~isempty(idx)           
+            patch_script.makepatch(GT_data,idx,bbox_intersection)
+       else
+           warning('Image Not found.');
+       end
+    end    
+% %elseif size(GT_data,1) == size(images,1)    
+%     for i=1:size(GT_data,1)
+%         patch_script.makepatch(GT_data,i,bbox_intersection)
+%     end
+% %else
+%     error('Number of Images in Big_Image folder do not match Size of GT.');
+% %end
+disp('Function Completed');
