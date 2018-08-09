b3420610b1cbb059b222ae2fe41b76dc28733af7
Deep Via23 <21269458+Deep419@users.noreply.github.com>
2018-08-09 17:12:53 -0400

    Refactored createSplit, patch_picker, dataset_splitter. About 100 times speedup

--- pollen_patch_picker_mc.m
+++ pollen_patch_picker_mc.m
@@ -1,46 +1,81 @@
-%% Dependencies : +patch_script\id_includer.m
-function gt_aug = pollen_patch_picker_mc (path, varargin)
+function gt_aug = pollen_patch_picker_mc (path,type,pattern,varargin)
 %%  This function will take input of which classes to consider, and will output
 %   a GT file with all of those classes combined in FRCNN format
+%
+%   if PATTERN = ALL, no varargin required
+%   if PATTERN = CONTINOUS, nargin is multiple of 2, first is start, second
+%       is end
+%   if PATTERN = DISCRETE, nargin is specific id's
 
+switch type
+    case 'GRI'
+        type_name = 'grid';
+    case 'PSO'
+        type_name = 'pso';
+    otherwise
+        error("Invalid 'Type' selected. Choices are 'GRI' for grid or 'PSO' for pso");
+end
+
+id_list = [];
+files = dir(fullfile(path,[type_name '_patches'],'*','*_aug.mat'));
+for i = 1:size(files,1)
+    load([files(i).folder filesep files(i).name])
+    id_name = strsplit(files(i).folder,filesep);
+    id_name = strsplit(id_name{end},'_');
+    id_list(i) = str2num(id_name{1});
+    files(i).id = str2num(id_name{1});
+end
+available_id_list = unique(id_list);
+
+switch pattern
+    % All picks all ids/class
+    case 'all'
+        gt_aug = pick_aug_data_from_patches(files);
+        
+    % Continous picks range of ids
+    case 'continous'
+        required_id_list = sort(cell2mat(reshape(varargin,[2 (nargin-3)/2])),2);
+        if ismember(required_id_list,available_id_list)
+            t = [];
+            for col = 1:size(required_id_list,2)
+                t = [t (required_id_list(1,col):required_id_list(2,col))];
+            end
+            required_id_list = t;
+            tabFiles = struct2table(files);
+            files = files(ismember(tabFiles.id, required_id_list));
+            gt_aug = pick_aug_data_from_patches(files);
+        else
+            error('Out of range ID inserted. Check your entered ids');
+        end
+        
+    case 'discrete'
+        required_id_list = sort(cell2mat(varargin));
+        if ismember(required_id_list,available_id_list)           
+            tabFiles = struct2table(files);
+            files = files(ismember(tabFiles.id, required_id_list));
+            gt_aug = pick_aug_data_from_patches(files);
+        else
+            error('Out of range ID inserted. Check your entered ids');
+        end
+    otherwise
+        error("Invalid 'Pattern' selected. Choices are 'all', 'continous', and 'discrete'");
+end
+
+gt_aug.imageFilename = fullfile(gt_aug.imageFilename);
+end
+
+function gt_aug = pick_aug_data_from_patches(files)
+warning off;
 gt_aug = table();
 sz_ctr = 0;
-if strcmp(varargin{1},'all') == 1    
-        files = dir(fullfile(path,'patches','*','*_aug.mat'));    
-    for i = 1:size(files,1)
-        load([files(i).folder filesep files(i).name])
-        id_name = strsplit(files(i).folder,filesep);
-        id_name = strsplit(id_name{end},'_');
-        id_name = ['id_' id_name{1}];
-        sz = height(data_aug);
-        gt_aug.imageFilename(sz_ctr+1:sz_ctr+sz,1) = data_aug.imageFilename;
-        gt_aug.(id_name)(sz_ctr+1:sz_ctr+sz,1) = data_aug.bbox;
-        sz_ctr = sz_ctr+ sz;
-    end
-else
-    list = patch_script.id_includer(varargin);
-    for i = 1:numel(list)
-        id_name = num2str(list(i));
-        folder = id_name;
-        if length(folder) == 1
-            folder = ['00' folder];
-        elseif length(folder) == 2
-            folder = ['0' folder];
-        end        
-        files = dir(fullfile(path,'patches',[folder '*'],'*_aug.mat'));
-        id_name = ['id_' id_name];
-        for ii = 1:size(files,1)
-            load([files(ii).folder filesep files(ii).name])
-            sz = height(data_aug);
-            gt_aug.imageFilename(sz_ctr+1:sz_ctr+sz,1) = data_aug.imageFilename;
-            gt_aug.(id_name)(sz_ctr+1:sz_ctr+sz,1) = data_aug.bbox;
-            sz_ctr = sz_ctr+ sz;
-        end
-    end
-end
-if isempty(strfind(gt_aug.imageFilename{1},'/'))
-    gt_aug.imageFilename = strrep(gt_aug.imageFilename,'\',filesep);
-else
-    gt_aug.imageFilename = strrep(gt_aug.imageFilename,'/',filesep);
+for i = 1:size(files,1)
+    load([files(i).folder filesep files(i).name])
+    id_name = strsplit(files(i).folder,filesep);
+    id_name = strsplit(id_name{end},'_');
+    id_name = ['id_' id_name{1}];
+    sz = height(data_aug);
+    gt_aug.imageFilename(sz_ctr+1:sz_ctr+sz,1) = data_aug.imageFilename;
+    gt_aug.(id_name)(sz_ctr+1:sz_ctr+sz,1) = data_aug.bbox;
+    sz_ctr = sz_ctr+ sz;
 end
 end
